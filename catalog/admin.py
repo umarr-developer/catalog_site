e8a2028426bd38c60804beae214b7f9c93d0c70a
umarr <umarr.developer@gmail.com>
2024-05-11 15:58:10 +0600

    add models Brand and LaptopProduct

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from catalog.models import Brand, LaptopProduct
+
 # Register your models here.
+admin.site.register(Brand)
+admin.site.register(LaptopProduct)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+
+class LaptopProduct(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    brand = models.ForeignKey(Brand, on_delete=models.DO_NOTHING)
+    description = models.TextField()
+    image = models.FileField(upload_to='files/')
+    cpu = models.CharField(max_length=128)
+    gpu = models.CharField(max_length=128)
+    ram = models.CharField(max_length=128)
+    memory = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+    create_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
