823906f6fc21c265b6a702ab6e1580ea98782384
umarr <umarr.developer@gmail.com>
2024-05-26 11:17:20 +0600

    add new modal daorm and fix scripts

--- catalog/static/js/scripts.js
+++ catalog/static/js/scripts.js
@@ -1,25 +1,25 @@
-const auth_modal = document.getElementById("auth_modal");
+const auth_form = document.getElementById("auth_form");
 const auth_button = document.getElementById("auth_button");
 const auth_close = document.getElementById("auth_close")
 
-const reg_modal = document.getElementById("reg_modal");
+const reg_form = document.getElementById("reg_form");
 const reg_button = document.getElementById("reg_button");
 const reg_close = document.getElementById("reg_close")
 
 
 auth_button.onclick = function () {
-    auth_modal.style.display = "block";
+    auth_form.style.display = "block";
 }
 
 auth_close.onclick = function () {
-    auth_modal.style.display = "none";
+    auth_form.style.display = "none";
 }
 
 
 reg_button.onclick = function (){
-    reg_modal.style.display = "block";
+    reg_form.style.display = "block";
 }
 
 reg_close.onclick = function () {
-    reg_modal.style.display = "none";
+    reg_form.style.display = "none";
 }
