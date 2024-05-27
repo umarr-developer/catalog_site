e61aacc345c5c0804d1203619efb2807486a5ed3
umarr <umarr.developer@gmail.com>
2024-05-28 03:51:58 +0600

    edit Product model and add new model ProductType

--- catalog/models.py
+++ catalog/models.py
@@ -10,10 +10,20 @@ class Brand(models.Model):
         return self.name
 
 
+class ProductType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=128)
     brand = models.ForeignKey(Brand, on_delete=models.DO_NOTHING)
+    ptype = models.ForeignKey(ProductType, on_delete=models.DO_NOTHING, default=None)
     description = models.TextField()
     image = models.FileField(upload_to='files/')
     characteristic = models.JSONField()
