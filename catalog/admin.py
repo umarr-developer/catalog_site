b3e4b45e1b85b9f9041ba940b63284ab9379065c
umarr <umarr.developer@gmail.com>
2024-05-27 11:22:06 +0600

    add new product models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from catalog.models import Brand, LaptopProduct
+from catalog.models import Brand, LaptopProduct, PhoneProduct, AccessoriesProduct
 
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(LaptopProduct)
+admin.site.register(PhoneProduct)
+admin.site.register(AccessoriesProduct)
--- catalog/models.py
+++ catalog/models.py
@@ -20,8 +20,35 @@ class LaptopProduct(models.Model):
     gpu = models.CharField(max_length=128)
     ram = models.CharField(max_length=128)
     memory = models.CharField(max_length=128)
+    price = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
     create_on = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class PhoneProduct(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    brand = models.ForeignKey(Brand, on_delete=models.DO_NOTHING)
+    description = models.TextField()
+    image = models.FileField(upload_to='files/')
+    cpu = models.CharField(max_length=128)
+    memory = models.CharField(max_length=128)
+    characteristic = models.JSONField()
+    price = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    create_on = models.DateField(auto_now_add=True)
+
+
+class AccessoriesProduct(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    brand = models.ForeignKey(Brand, on_delete=models.DO_NOTHING)
+    description = models.TextField()
+    image = models.FileField(upload_to='files/')
+    characteristic = models.JSONField()
+    price = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    create_on = models.DateField(auto_now_add=True)
