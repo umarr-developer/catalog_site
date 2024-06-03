2620bb1a4928d2f5943c6579589b16feb18b57f1
umarr <umarr.developer@gmail.com>
2024-06-03 10:19:47 +0600

    add searchPageView and template

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from catalog.views import MainPageView, ProductPageView, InProgress
+from catalog.views import MainPageView, ProductPageView, InProgress, SearchPageView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page'),
     path('product/<slug:slug>', ProductPageView.as_view(), name='product_view'),
+    path('search/', SearchPageView.as_view(), name='search'),
     path('in_progress/', InProgress.as_view(), name='in_progress')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.shortcuts import render
 from django.views import generic
 
 from catalog.models import Product
@@ -15,6 +17,23 @@ class MainPageView(generic.TemplateView):
 class ProductPageView(generic.DetailView):
     model = Product
     template_name = 'product.html'
+
+
+class SearchPageView(generic.ListView):
+    template_name = 'search.html'
+    model = Product
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        query_value = request.GET.get('value')
+        search_result = Product.objects.filter(
+            Q(name__icontains=query_value) |
+            Q(description__icontains=query_value)
+        )
+
+        context['search_products'] = search_result
+
+        return render(request, self.template_name, context)
 
 
 class InProgress(generic.TemplateView):
