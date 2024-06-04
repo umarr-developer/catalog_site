cfde100d581537d8d61f0e36eb06c44c5a9337bc
umarr <umarr.developer@gmail.com>
2024-06-04 11:11:28 +0600

    register model ProductType

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from catalog.models import Brand, Product
+from catalog.models import Brand, Product, ProductType
 
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(Product)
+admin.site.register(ProductType)
